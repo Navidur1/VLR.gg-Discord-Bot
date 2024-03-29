b1e84d0faf5ca0afa5c5f3b50a2cd325e5d8c934
Navidur Rahman <navid2001@gmail.com>
2024-03-29 18:23:19 -0400

    testing

--- bot.py
+++ bot.py
@@ -6,29 +6,32 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def getCall(req):
+
+def get_call(req):
     response = requests.get('https://vlrggapi.herokuapp.com/news')
     jsonData = json.loads(response.text)
     return jsonData['data']['segments'][0]['description']
+
 
 client = discord.Client()
 
 
 @client.event
-async def onReady():
+async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
-async def onMessage(message):
+async def on_message(message):
     if message.author == client.user:
         return
 
     if message.content == ('/news'):
-        msg = getCall('/news')
+        msg = get_call('/news')
         await message.channel.send(msg)
 
     if message.content == ('/matches/results'):
-        msg = getCall('/matches/results')
+        msg = get_call('/matches/results')
         await message.channel.send(msg)
 
 client.run(os.getenv('TOKEN'))
--- scrape.py
+++ scrape.py
@@ -5,14 +5,14 @@ URL = "https://www.vlr.gg/77797/xset-vs-cloud9-champions-tour-north-america-stag
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
-header = soup.find(class_ = "match-header-vs")
+header = soup.find(class_="match-header-vs")
 
-teamHeader = soup.findAll(class_ = "wf-title-med")
-team1 = teamHeader[0].text.strip()
-team2 = teamHeader[1].text.strip()
+team_header = soup.findAll(class_="wf-title-med")
+team1 = team_header[0].text.strip()
+team2 = team_header[1].text.strip()
 
-scoreHeader = soup.find(class_ = "js-spoiler").contents
-team1Score = scoreHeader[1].text.strip()
-team2Score = scoreHeader[5].text.strip()
+score_header = soup.find(class_="js-spoiler").contents
+team1Score = score_header[1].text.strip()
+team2Score = score_header[5].text.strip()
 
 print(team1, team1Score + ":" + team2Score, team2)
